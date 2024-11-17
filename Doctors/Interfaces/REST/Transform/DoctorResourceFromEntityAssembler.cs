bc67725fd27bc789d2953c1eec1317e687102a08
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:41:24 -0500

    chore:fix comment

--- Doctors/Interfaces/REST/Transform/DoctorResourceFromEntityAssembler.cs
+++ Doctors/Interfaces/REST/Transform/DoctorResourceFromEntityAssembler.cs
@@ -3,6 +3,9 @@ using HealMeAppBackend.API.Doctors.Interfaces.REST.Resources;
 
 namespace HealMeAppBackend.API.Doctors.Interfaces.REST.Transform
 {
+     /// <summary>
+    ///     Assembler to transform a <see cref="Doctor"/> entity into a <see cref="DoctorResource"/>.
+    /// </summary>
     public static class DoctorResourceFromEntityAssembler
     {
         public static DoctorResource ToResourceFromEntity(Doctor entity) =>

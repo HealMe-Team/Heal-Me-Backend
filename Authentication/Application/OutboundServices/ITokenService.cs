4bbdbcaca837a0ce0855b0732eb3a37051641215
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:48:52 -0500

    chore:fix commit

--- Authentication/Application/OutboundServices/ITokenService.cs
+++ Authentication/Application/OutboundServices/ITokenService.cs
@@ -1,5 +1,12 @@
 ﻿namespace HealMeAppBackend.API.Authentication.Application.OutboundServices
 {
+    /// <summary>
+    ///     Token service interface.
+    /// </summary>
+    /// <remarks>
+    ///     This interface defines the basic operations for managing tokens, 
+    ///     including token generation and validation.
+    /// </remarks>
     public interface ITokenService
     {
         string GenerateToken(int userId, string username);

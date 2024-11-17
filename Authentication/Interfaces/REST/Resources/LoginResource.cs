dd4f7027b3d498f29e9e94312754ea896fbdccf3
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:52:24 -0500

    chore:fix commit

--- Authentication/Interfaces/REST/Resources/LoginResource.cs
+++ Authentication/Interfaces/REST/Resources/LoginResource.cs
@@ -1,5 +1,12 @@
 ﻿namespace HealMeAppBackend.API.Authentication.Interfaces.REST.Resources
 {
+    /// <summary>
+    ///     Resource representing the login credentials of a user.
+    /// </summary>
+    /// <remarks>
+    ///     This class is used to encapsulate the username and password 
+    ///     provided by the user during the authentication process.
+    /// </remarks>
     public class LoginResource
     {
         public string Username { get; set; }

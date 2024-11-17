335a8887ac285b10486786964a9679d03a4e61b7
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 18:01:29 -0500

    chore:fix commit

--- Hospitals/Interfaces/REST/Transform/HospitalResourceFromEntityAssembler.cs
+++ Hospitals/Interfaces/REST/Transform/HospitalResourceFromEntityAssembler.cs
@@ -3,6 +3,13 @@ using HealMeAppBackend.API.Hospitals.Interfaces.REST.Resources;
 
 namespace HealMeAppBackend.API.Hospitals.Interfaces.REST.Transform
 {
+     /// <summary>
+    ///     Assembler to transform a hospital entity into a resource.
+    /// </summary>
+    /// <remarks>
+    ///     This static class provides a method to convert a <see cref="Hospital"/> entity 
+    ///     into a <see cref="HospitalResource"/> used by the REST interface.
+    /// </remarks>
     public static class HospitalResourceFromEntityAssembler
     {
         public static HospitalResource ToResourceFromEntity(Hospital entity) =>

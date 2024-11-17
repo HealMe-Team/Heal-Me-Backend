4e93e8a353e78f7f207f205c90a37c94d4bc6d53
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:53:22 -0500

    chore:fix commit

--- Authentication/Interfaces/REST/Resources/RegisterResource.cs
+++ Authentication/Interfaces/REST/Resources/RegisterResource.cs
@@ -1,5 +1,12 @@
 ﻿namespace HealMeAppBackend.API.Authentication.Interfaces.REST.Resources
 {
+    /// <summary>
+    ///     Resource representing the registration details of a new user.
+    /// </summary>
+    /// <remarks>
+    ///     This class is used to encapsulate the information required for user registration, 
+    ///     including a username and a password.
+    /// </remarks>
     public class RegisterResource
     {
         public string Username { get; set; }

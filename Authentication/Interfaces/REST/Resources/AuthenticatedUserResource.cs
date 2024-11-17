2933e27f1bb9f639124b15c230378435864f977a
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:51:03 -0500

    chore:fix commit

--- Authentication/Interfaces/REST/Resources/AuthenticatedUserResource.cs
+++ Authentication/Interfaces/REST/Resources/AuthenticatedUserResource.cs
@@ -1,5 +1,12 @@
 ﻿namespace HealMeAppBackend.API.Authentication.Interfaces.REST.Resources
 {
+    /// <summary>
+    ///     Resource representing an authenticated user.
+    /// </summary>
+    /// <remarks>
+    ///     This class encapsulates the details of an authenticated user, 
+    ///     including their username and the authentication token.
+    /// </remarks>
     public class AuthenticatedUserResource
     {
         public string Username { get; set; }

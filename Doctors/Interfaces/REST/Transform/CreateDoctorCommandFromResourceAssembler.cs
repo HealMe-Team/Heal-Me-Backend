bc6e174380479f8b54c7be59626334b0391b8d95
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 17:39:23 -0500

    chore:fix comment

--- Doctors/Interfaces/REST/Transform/CreateDoctorCommandFromResourceAssembler.cs
+++ Doctors/Interfaces/REST/Transform/CreateDoctorCommandFromResourceAssembler.cs
@@ -3,6 +3,9 @@ using HealMeAppBackend.API.Doctors.Interfaces.REST.Resources;
 
 namespace HealMeAppBackend.API.Doctors.Interfaces.REST.Transform
 {
+    /// <summary>
+    ///     Assembler to transform a <see cref="CreateDoctorResource"/> into a <see cref="CreateDoctorCommand"/>.
+    /// </summary>
     public static class CreateDoctorCommandFromResourceAssembler
     {
         public static CreateProductCommand ToCommandFromResource(CreateDoctorResource resource) =>

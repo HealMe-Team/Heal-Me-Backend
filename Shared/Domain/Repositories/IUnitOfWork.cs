53fce34f1ccc5fd8424ae5d6e9ed55ad9221ede4
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 18:21:52 -0500

    chore:fix commit

--- Shared/Domain/Repositories/IUnitOfWork.cs
+++ Shared/Domain/Repositories/IUnitOfWork.cs
@@ -1,5 +1,12 @@
 ﻿namespace HealMeAppBackend.API.Shared.Domain.Repositories
 {
+    /// <summary>
+    ///     Defines the contract for a unit of work pattern.
+    /// </summary>
+    /// <remarks>
+    ///     The unit of work pattern is used to group multiple operations 
+    ///     into a single transaction, ensuring consistency and reducing overhead.
+    /// </remarks>
     public interface IUnitOfWork
     {
         Task CompleteAsync();

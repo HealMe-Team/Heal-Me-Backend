bf6255c46979f05954b297206e71df3bb2819c4a
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 18:00:56 -0500

    chore:fix commit

--- Hospitals/Interfaces/REST/Transform/CreateHospitalCommandFromResourceAssembler.cs
+++ Hospitals/Interfaces/REST/Transform/CreateHospitalCommandFromResourceAssembler.cs
@@ -3,6 +3,13 @@ using HealMeAppBackend.API.Hospitals.Interfaces.REST.Resources;
 
 namespace HealMeAppBackend.API.Hospitals.Interfaces.REST.Transform
 {
+    /// <summary>
+    ///     Assembler to transform a resource into a hospital creation command.
+    /// </summary>
+    /// <remarks>
+    ///     This static class provides a method to convert a `CreateHospitalResource` 
+    ///     into a `CreateHospitalCommand` used by the domain layer.
+    /// </remarks>
     public static class CreateHospitalCommandFromResourceAssembler
     {
         public static CreateHospitalCommand ToCommandFromResource(CreateHospitalResource resource) =>

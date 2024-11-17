4c166e3b29e07f5edd24b9624aecdb438d721f41
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 18:09:33 -0500

    chore:fix commit

--- Products/Interfaces/REST/Transform/CreateProductCommandFromResourceAssembler.cs
+++ Products/Interfaces/REST/Transform/CreateProductCommandFromResourceAssembler.cs
@@ -3,6 +3,13 @@ using HealMeAppBackend.API.Products.Interfaces.REST.Resources;
 
 namespace HealMeAppBackend.API.Products.Interfaces.REST.Transform
 {
+     /// <summary>
+    ///     Repository implementation for managing products in the database.
+    /// </summary>
+    /// <remarks>
+    ///     Provides methods to query and manage product data, including finding products
+    ///     by name, ID, and rating.
+    /// </remarks>
     public static class CreateProductCommandFromResourceAssembler
     {
         public static CreateProductCommand ToCommandFromResource(CreateProductResource resource) =>

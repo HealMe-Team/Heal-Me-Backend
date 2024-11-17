509ca91970b341addace5a46508501830b851501
Luis Angel Cisneros salas <104966556+LuisCS341@users.noreply.github.com>
2024-11-17 18:14:18 -0500

    chore:fix commit

--- Products/Interfaces/REST/ProductsController.cs
+++ Products/Interfaces/REST/ProductsController.cs
@@ -10,6 +10,13 @@ using Swashbuckle.AspNetCore.Annotations;
 
 namespace HealMeAppBackend.API.Products.Interfaces.REST;
 
+/// <summary>
+///     Products controller.
+/// </summary>
+/// <remarks>
+///     This class provides endpoints for managing products, including creation, retrieval, and filtering by specific attributes.
+/// </remarks>
+
 [ApiController]
 [Route("api/v1/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
@@ -18,6 +25,14 @@ public class ProductsController(
     IProductCommandService productCommandService,
     IProductQueryService productQueryService) : ControllerBase
 {
+    /// <summary>
+    ///     Creates a new product.
+    /// </summary>
+    /// <remarks>
+    ///     This endpoint creates a product using the provided name, description, price, and rating.
+    /// </remarks>
+    /// <param name="resource">The product creation resource containing the product details.</param>
+    /// <returns>The created product resource, including its generated ID.</returns>
     [HttpPost]
     [Authorize]
     [SwaggerOperation(
@@ -36,6 +51,14 @@ public class ProductsController(
         return CreatedAtAction(nameof(GetProductById), new { id = result.Id }, ProductResourceFromEntityAssembler.ToResourceFromEntity(result));
     }
 
+    /// <summary>
+    ///     Retrieves a product by name.
+    /// </summary>
+    /// <remarks>
+    ///     This endpoint retrieves a product based on the given name.
+    /// </remarks>
+    /// <param name="name">The name of the product to retrieve.</param>
+    /// <returns>The product resource corresponding to the specified name.</returns>
     [HttpGet]
     [Authorize]
     [SwaggerOperation(
@@ -53,6 +76,14 @@ public class ProductsController(
         return Ok(resource);
     }
 
+    /// <summary>
+    ///     Retrieves a product by ID.
+    /// </summary>
+    /// <remarks>
+    ///     This endpoint retrieves a product using its unique identifier.
+    /// </remarks>
+    /// <param name="id">The ID of the product to retrieve.</param>
+    /// <returns>The product resource corresponding to the specified ID.</returns>
     [HttpGet("{id}")]
     [Authorize]
     [SwaggerOperation(
@@ -70,6 +101,14 @@ public class ProductsController(
         return Ok(resource);
     }
 
+    /// <summary>
+    ///     Retrieves products by rating.
+    /// </summary>
+    /// <remarks>
+    ///     This endpoint retrieves all products that match the specified rating.
+    /// </remarks>
+    /// <param name="rating">The rating value to filter products.</param>
+    /// <returns>A list of products with the specified rating.</returns>
     [HttpGet("rating/{rating}")]
     [Authorize]
     [SwaggerOperation(
